a55d0e74059af9d6f40c608a934079c3b0a248f0
Tomthy <a82618099@gmail.com>
2015-06-07 22:26:49 +0800

    Create the frame

---
+++ TosCalculator.java
@@ -0,0 +1,12 @@
+public class TosCalculator extends JFrame{
+	public static void main(String[] args){
+		
+		JFrame frame = new DamageCalculate();
+		frame.setTitle("U10316039_TOSCalculator");
+		frame.pack();
+		frame.setLocationRelativeTo(null); // Center the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
